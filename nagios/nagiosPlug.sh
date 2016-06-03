9dfd6efdecd38600e4ba06fdd8e428bc948c6f18
mingyagu <mingyagu@adcloudsystem.com>
2016-06-03 14:15:34 +0900

    .

--- nagios/nagiosPlug.sh
+++ nagios/nagiosPlug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /tmp/nagios-plugins-2.1.1
+cd $STIGMA_WORK/nagios-plugins-2.1.1
 ./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
 make all
 make install
